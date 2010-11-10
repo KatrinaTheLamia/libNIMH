3c631efbb288d8231bf0aad206673e4489ddfbe5
Katrina Payne <FullMetalHarlot@NIMHLabs.Com>
2010-11-10 07:44:38 -0700

    hey, something real to grab all out files from... WOOHOO

---
+++ Includes/libnimh.h
@@ -0,0 +1,45 @@
+/*nimh-doc
+ * = %(libnimh-includes)path/libnimh.h
+ * @Author : %(KatrinaTheLamia)user
+ * @Groups : %(NIMHLabs)group, %(SpectrumLabs)group
+ * @Projects: %(libNIMH)project
+ * @Creation: 3176-5-22
+ * @License : %(libnimh-documentation)path/LICENSE.txt
+ *
+ * Mostly just a standard header, to allow libNIMH to be used in various
+ * other applications (provided they link to libNIMH.so or libNIMH.dll)
+ *
+ * == Revisions
+ * + 3176-5-22 File creation
+ *
+ * == TODO
+ * ! Actually make these headers usable
+ *
+ */
+
+#ifndef __libNIMH_H__
+#define __libNIMH_H__
+
+/* Start up our C++ shields  */
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* C++ shields up and at full power, captain */
+
+#include <configuration.h>
+#include <nimh_string.h>
+#include <nimh_time.h>
+#include <widget.h>
+#include <threading.h>
+#include <storage.h>
+#include <dynamic_loading.h>
+#include <memory_management.h>
+
+
+/* Drop our C++ shileds  */
+#ifdef __cplusplus
+};
+#endif
+#endif /* __libNIMH_H__ is teh over */
+
+
